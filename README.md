03bb071fd01d6dad5214d36b1d6f3646bbba3e56
Marek Bielecki <marekbielecki93@gmail.com>
2018-03-10 19:20:09 +0100

    block and hash calculator

---
+++ README.md
@@ -0,0 +1,8 @@
+# Naivechain
+
+Very simple blockchain implementation.
+
+## References
+
+* https://medium.com/@lhartikk/a-blockchain-in-200-lines-of-code-963cc1cc0e54
+* https://github.com/azophy/naivechain-php
---
+++ src/Block.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Naivechain;
+
+/**
+ * Class Block
+ * @package Naivechain
+ */
+class Block
+{
+    /**
+     * @var integer
+     */
+    private $index;
+
+    /**
+     * @var string
+     */
+    private $previousHash;
+
+    /**
+     * @var string
+     */
+    private $hash;
+
+    /**
+     * @var \DateTimeInterface
+     */
+    private $timestamp;
+
+    /**
+     * @var string
+     */
+    private $data;
+
+    /**
+     * Block constructor.
+     *
+     * @param int $index
+     * @param string $previousHash
+     * @param string $hash
+     * @param \DateTimeInterface $timestamp
+     * @param string $data
+     */
+    public function __construct(
+        int $index,
+        string $previousHash,
+        string $hash,
+        \DateTimeInterface $timestamp,
+        string $data
+    ) {
+        $this->index = $index;
+        $this->previousHash = $previousHash;
+        $this->hash = $hash;
+        $this->timestamp = $timestamp;
+        $this->data = $data;
+    }
+
+    public static function createFromArray(array $array): Block
+    {
+        return new static(
+            $array['index'],
+            $array['previousHash'],
+            $array['hash'],
+            $array['timestamp'],
+            $array['data']
+        );
+    }
+
+    /**
+     * @return int
+     */
+    public function getIndex(): int
+    {
+        return $this->index;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPreviousHash(): string
+    {
+        return $this->previousHash;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHash(): string
+    {
+        return $this->hash;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getTimestamp(): \DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getData(): string
+    {
+        return $this->data;
+    }
+}
